3233cb04ed3345d7ba9755ec1985baae6d78382b
cjhwan6 <cjhwan6@hanmail.net>
2015-09-02 17:09:33 +0900

    서버 프로젝트를 웹으로 변경

--- ChatServer/src/com/core/ChatEndPoint.java
+++ ChatServer/src/com/core/ChatEndPoint.java
@@ -1,13 +1,10 @@
 package com.core;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
-import javax.websocket.EncodeException;
-import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -18,76 +15,50 @@ import javax.websocket.server.ServerEndpoint;
 
 
 @ServerEndpoint(value = "/ChatMess")
-public class ChatEndPoint extends Endpoint {
+public class ChatEndPoint  {
 	
-	private Map<String,LinkedList<Session>> clientMap = new HashMap<String,LinkedList<Session>>();
+    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
 	
 	@OnOpen
 	public void onOpen(Session s, EndpointConfig e) {
-		String key = String.valueOf(s.getUserProperties().get("diagKey"));
-		LinkedList<Session> lstClient = clientMap.get(key);
-		lstClient.add(s);
+		peers.add(s);
 	}
 	
 	@OnClose
-	public void onClose(Session s){
-		String key = String.valueOf(s.getUserProperties().get("diagKey"));
-		LinkedList<Session> lstClient = clientMap.get(key);
-		lstClient.remove(s);
+	public void onClose(Session s){		
+		peers.remove(s);
 	}
-	
-	
-	/**
-	 * 메세지 받을때 사용
-	 * @param message
-	 * @param clientId
-	 */
-	@OnMessage
-	public void handleMessage(String message,Session sess){
-		String key = "";
-		LinkedList<Session> lstClient = clientMap.get(key);
-		// 메세지 수신시, 관련된 Client들에게 모두 보냄
-		for(Session client : lstClient){
-			try {
-				client.getBasicRemote().sendText(message);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 		
-	}
-	
 	/**
 	 * 대화상대 조회시 사용
 	 * @param message
 	 * @param clientId
 	 */
 	@OnMessage
-	public void onMessage(Object message,String clientId){
+	public void onMessage(String message,String clientId){
 		ArrayList<String> rtnClient = new ArrayList<String>();
 		Session tarClient = null;
-		for(String key : clientMap.keySet() ){
-			LinkedList<Session> lstClient = clientMap.get(key);
-			// 객체 메세지 수신시, 접속된 Client들 정보를 모두 반환함.
-			for(Session client : lstClient){
-				String strName = String.valueOf(client.getUserProperties().get("clientName"));
-				rtnClient.add(strName);
-				if(strName.equals(clientId)){
-					tarClient = client;
-				}
-			}			
-		}		
-		if(tarClient == null){
-			return;
-		}
-		// 요청대상에 반환
-		try {
-			tarClient.getBasicRemote().sendObject(rtnClient);
-		} catch (IOException | EncodeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		for(String key : clientMap.keySet() ){
+//			LinkedList<Session> lstClient = clientMap.get(key);
+//			// 객체 메세지 수신시, 접속된 Client들 정보를 모두 반환함.
+//			for(Session client : lstClient){
+//				String strName = String.valueOf(client.getUserProperties().get("clientName"));
+//				rtnClient.add(strName);
+//				if(strName.equals(clientId)){
+//					tarClient = client;
+//				}
+//			}			
+//		}		
+//		if(tarClient == null){
+//			return;
+//		}
+//		// 요청대상에 반환
+//		try {
+//			tarClient.getBasicRemote().sendObject(rtnClient);
+//		} catch (IOException | EncodeException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
 	
